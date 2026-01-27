cf81d985e39f81e25dcf266578914fe3f065d398
Marnix Dessing <m.dessing@nijmegen.nl>
2026-01-27 12:13:22 +0100

    chore: projen project not devDep but dep

--- .projenrc.js
+++ .projenrc.js
@@ -34,6 +34,7 @@ const project = new GemeenteNijmegenCdkApp({
     '@types/aws-lambda',
     'chart.js',
     '@privacybydesign/yivi-frontend',
+    '@gemeentenijmegen/projen-project-type',
   ],
   devDeps: [
     'copyfiles',
@@ -42,7 +43,6 @@ const project = new GemeenteNijmegenCdkApp({
     '@glen/jest-raw-loader',
     'axios-mock-adapter',
     'jest-aws-client-mock',
-    '@gemeentenijmegen/projen-project-type',
     'copyfiles',
   ],
   jestOptions: {
