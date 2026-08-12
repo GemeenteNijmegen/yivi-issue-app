5cca083faa80fc1f59fa258406ad45c0189eff84
WietekeNijmegen <wietekenijmegen@gmail.com>
2026-08-12 18:03:15 +0200

    fix: add no email records to production after accp tests - in config added

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -138,6 +138,7 @@ const configurations: { [name: string]: Configuration } = {
     cnameRecords: {
       _e573bcd00b0f468178ff502aeb92eae3: '_df939a5caaba3eef9055e611864019d2.yghrkwvzvz.acm-validations.aws.',
     },
+    noMailRecords: true,
     criticality: new Criticality('high'),
     useHaalCentraalBrp: true,
     sdJwtBatchSize: 50, // Yivi default value
